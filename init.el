d6a18dd5b30394a23f150e2202eae410d3dd57e3
Yuma Arakawa <yuarkw@gmail.com>
2016-07-14 11:09:31 +0900

    add sh-mode indent settings

--- init.el
+++ init.el
@@ -144,6 +144,24 @@
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "~/firefox/firefox")
 
+;; sh-modeの設定
+;; https://keramida.wordpress.com/2008/08/08/tweaking-shell-script-indentation-in-gnu-emacs/
+(defun gker-setup-sh-mode ()
+  "My own personal preferences for `sh-mode'.
+
+This is a custom function that sets up the parameters I usually
+prefer for `sh-mode'.  It is automatically added to
+`sh-mode-hook', but is can also be called interactively."
+  (interactive)
+  (setq sh-basic-offset 8
+        sh-indentation 8
+        ;; Tweak the indentation level of case-related syntax elements, to avoid
+        ;; excessive indentation because of the larger than default value of
+        ;; `sh-basic-offset' and other indentation options.
+        sh-indent-for-case-label 0
+        sh-indent-for-case-alt '+))
+(add-hook 'sh-mode-hook 'gker-setup-sh-mode)
+
 ;======================================================================
 ; 追加外部スクリプトに関する設定(Caskで管理)
 ;======================================================================
